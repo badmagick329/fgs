11f6e1dbf18ff0308585b64f37a22ab7fb924505
badmagick329 <uzairfarooqi@gmail.com>
2026-02-04 09:45:33 +0000

    refactor: email worker
    move worker interval value to .env file. making it easier to tweak this
    value without changing code. move checkEmail logic out into a function
    and call it on startup. don't send emails if no pending emails are
    found.

--- email_worker/index.ts
+++ email_worker/index.ts
@@ -1,20 +1,49 @@
-import { getPendingEmails } from '../frontend/src/lib/db.ts'
-import { sendEmail } from '../frontend/src/actions/email-actions.ts'
+import { getPendingEmails } from "../frontend/src/lib/db.ts";
+import { sendEmail } from "../frontend/src/actions/email-actions.ts";
 
 async function main() {
-  console.log(`[${new Date().toISOString()}] - Starting email worker`)
-  const interval = setInterval(() => {
-    try {
-      getPendingEmails().then((emails) => {
-        console.log(
-          `[${new Date().toISOString()}] - Fetched ${emails.length} pending emails`,
-        )
-        sendEmail({ email_data: emails })
-      })
-    } catch (error) {
-      console.error('Error in getPendingEmails interval:', error)
-    }
-  }, 60 * 1000) // every 60 seconds at the moment !!!!!! CHANGE THIS FOR LIVE !!!!!!!!!
+  const workerInterval = getWorkerInterval();
+  if (!workerInterval) return process.exit(1);
+
+  await checkEmails();
+  console.log(`[${new Date().toISOString()}] - Starting email worker`);
+  setInterval(checkEmails, workerInterval);
 }
 
-main()
+function getWorkerInterval() {
+  const workerInterval = parseInt(process.env.WORKER_INTERVAL || "0");
+  if (isNaN(workerInterval) || workerInterval <= 1000) {
+    console.error(`Invalid WORKER_INTERVAL value: ${workerInterval}`);
+    return null;
+  }
+  if (workerInterval < 1000 * 60 * 10) {
+    console.warn(
+      `WORKER_INTERVAL is set to a low value (${workerInterval} ms). This may lead to rate limiting by the email provider.`,
+    );
+  }
+  return workerInterval;
+}
+
+async function checkEmails() {
+  console.log(`[${new Date().toISOString()}] - Checking emails`);
+
+  try {
+    const emails = await getPendingEmails();
+
+    if (emails.length > 0) {
+      console.log(
+        `[${new Date().toISOString()}] - Fetched ${emails.length} pending emails`,
+      );
+      sendEmail({ email_data: emails });
+      return;
+    }
+    console.log(`[${new Date().toISOString()}] - No pending emails found`);
+  } catch (error) {
+    console.error("Error in checkEmails", error);
+  }
+}
+
+main().catch((error) => {
+  console.error("Fatal error in email worker", error);
+  process.exit(1);
+});
